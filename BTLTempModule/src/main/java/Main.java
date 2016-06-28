6b05b70ad2013a9c1530a6a4f68b35941f812645
MuH3gPaB <figurin_mih@mail.ru>
2016-06-28 12:51:29 +0300

    Причесал структуру проекта

--- BTLTempModule/src/main/java/Main.java
+++ BTLTempModule/src/main/java/Main.java
@@ -1,9 +1,9 @@
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.metadata.ClassMetadata;
+//import org.hibernate.HibernateException;
+//import org.hibernate.Query;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.cfg.Configuration;
+//import org.hibernate.metadata.ClassMetadata;
 //import org.hibernate.service.ServiceRegistry;
 //import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
